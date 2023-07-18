5a1b7c6805342847c9970c2c13c1afe79b7f5e89
JoeSpizz <spizjo01@gmail.com>
2023-07-18 15:24:09 +0000

    manipulating lists

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append("dns") # this line will add "dns" to the end of our list
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
+addin = "Interloper on 3"
+proto.insert(3, addin)
+print(proto)
+
+proto.remove("Interloper on 3")
+print("interloper removed")
+print(proto)
+
+
+
+
